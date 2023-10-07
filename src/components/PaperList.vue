1feeb27f9e9e8b38f00b2b1d2a7907f4afe08feb
Devin Lange <devinscottlange@gmail.com>
2023-10-07 11:17:36 -0600

    refine list formatting

--- src/components/PaperList.vue
+++ src/components/PaperList.vue
@@ -5,25 +5,27 @@ const paperDataStore = usePaperDataStore();
 
 <template>
   <q-list v-if="paperDataStore.allData">
-    <q-item v-for="paperInfo in paperDataStore.allData" :key="paperInfo.doi">
-      <q-item-section>
-        <q-item-label>{{ paperInfo.title }}</q-item-label>
-        <q-item-label caption lines="2">{{
-          paperInfo.authorNames
-        }}</q-item-label>
-      </q-item-section>
+    <template v-for="paperInfo in paperDataStore.allData" :key="paperInfo.doi">
+      <q-item>
+        <q-item-section>
+          <q-item-label>{{ paperInfo.title }}</q-item-label>
+          <q-item-label caption lines="2">{{
+            paperDataStore.getAuthors(paperInfo).join(', ')
+          }}</q-item-label>
+        </q-item-section>
 
-      <q-item-section side top>
-        <q-item-label caption>{{
-          `${paperInfo.conference}, ${paperInfo.year}`
-        }}</q-item-label>
-        <span v-if="paperInfo.award">
-          <span>{{ paperInfo.award }} </span>
-          <q-icon name="emoji_events" color="primary" size="xs" />
-        </span>
-      </q-item-section>
-    </q-item>
-    <q-separator spaced inset />
+        <q-item-section side top>
+          <q-item-label caption>{{
+            `${paperDataStore.getConference(paperInfo)}, ${paperInfo.year}`
+          }}</q-item-label>
+          <q-item-label v-if="paperInfo.award"
+            >{{ paperDataStore.getAward(paperInfo) }}
+            <q-icon name="emoji_events" color="primary" size="xs" />
+          </q-item-label>
+        </q-item-section>
+      </q-item>
+      <q-separator spaced inset />
+    </template>
   </q-list>
 </template>
 
--- src/stores/paperDataStore.ts
+++ src/stores/paperDataStore.ts
@@ -6,6 +6,7 @@ import { parse, type ParseResult } from 'papaparse';
 export interface PaperInfo {
   title: string;
   authorNames: string;
+  authorNamesDeduped: string;
   doi: string;
   year: number;
   abstract: string;
@@ -20,10 +21,32 @@ export interface PaperDataStoreState {
 export const usePaperDataStore = defineStore('paperDataStore', () => {
   const allData = ref<PaperInfo[] | null>(null);
 
-  function transformHeader(header: string, _index: number): string {
-    if (header === 'DOI') return 'doi';
-    if (header === 'authorNames-Deduped') return 'authorNamesDeduped';
-    return header.slice(0, 1).toLowerCase() + header.slice(1);
+  function getAward(paperInfo: PaperInfo): string {
+    const key = paperInfo.award;
+    if (key === 'HM') return 'Honorable Mention';
+    if (key === 'TT') return 'Test of Time';
+    if (key === 'BP') return 'Best Paper';
+    return 'Unknown Award';
+  }
+
+  function getConference(paperInfo: PaperInfo): string {
+    const key = paperInfo.conference;
+    if (key === 'Vis') return 'VIS';
+    return key;
+  }
+
+  function getAuthors(paperInfo: PaperInfo): string[] {
+    const authors = paperInfo.authorNamesDeduped;
+    if (authors == null) {
+      return [];
+    }
+    const authorList = authors.split(';').map((name: string) => {
+      const nameChunks = name.split(' ').filter((chunk: string) => {
+        return isNaN(parseInt(chunk));
+      });
+      return nameChunks.join(' ');
+    });
+    return authorList;
   }
 
   parse(window.location.origin + '/data/VIS.csv', {
@@ -40,5 +63,11 @@ export const usePaperDataStore = defineStore('paperDataStore', () => {
     },
   });
 
-  return { allData };
+  function transformHeader(header: string, _index: number): string {
+    if (header === 'DOI') return 'doi';
+    if (header === 'AuthorNames-Deduped') return 'authorNamesDeduped';
+    return header.slice(0, 1).toLowerCase() + header.slice(1);
+  }
+
+  return { allData, getAward, getConference, getAuthors };
 });
