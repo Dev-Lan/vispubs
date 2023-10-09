779418bb7644dfd0aa0083870db85e0b5e4a3342
Devin Lange <devinscottlange@gmail.com>
2023-10-09 17:47:53 -0600

    add basic about section

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -1,5 +1,23 @@
 <template>
-  <q-page class="row items-center justify-evenly"> About Page Stub </q-page>
+  <q-page-container>
+    <q-page>
+      <div class="q-ma-lg">
+        <p>
+          This website is a personal project to create a single website that
+          contains all visualization publications. If you would like to have
+          your name linked to your personal website, fill out
+          <a href="https://forms.gle/WuXG2ZSidd9A9Ns57">this form</a>.
+        </p>
+
+        <p>
+          If you notice any mistakes, or would like to make a suggestion, please
+          <a href="https://github.com/Dev-Lan/vispubs/issues/new"
+            >let me know here.</a
+          >
+        </p>
+      </div>
+    </q-page>
+  </q-page-container>
 </template>
 
 <script setup lang="ts"></script>
