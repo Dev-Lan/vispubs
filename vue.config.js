5a4fcf364fa74c41cbd3a9297aa3946fd03f9b41
Devin Lange <devinscottlange@gmail.com>
2023-10-12 22:54:04 -0600

    try to fix mime type deploy error again

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,6 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
+  publicPath:
+    process.env.NODE_ENV === 'production'
+      ? 'https://www.vispubs.devinlange.com/'
+      : '/',
 };
