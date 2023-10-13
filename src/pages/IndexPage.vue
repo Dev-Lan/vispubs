3030c196866c4f2d2c35793d1507d78a0a1ffb79
Devin Lange <devinscottlange@gmail.com>
2023-10-12 19:48:48 -0600

    add swipe/keyboard controls

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { ref, computed, watch, onMounted } from 'vue';
 import { useElementSize } from '@vueuse/core';
+import { useKeypress } from 'vue3-keypress';
 
 import { usePaperDataStore } from 'src/stores/paperDataStore';
 const paperDataStore = usePaperDataStore();
@@ -13,6 +14,29 @@ const rightDrawerOpen = computed(() => paperDataStore.selectedPaper !== null);
 const container = ref(null);
 const { width: containerWidth, height: outerContainerHeight } =
   useElementSize(container);
+
+useKeypress({
+  keyEvent: 'keydown',
+  keyBinds: [
+    {
+      keyCode: 'left',
+      success: previousPaper,
+    },
+    {
+      keyCode: 'right',
+      success: nextPaper,
+    },
+  ],
+});
+
+function previousPaper() {
+  if (paperDataStore.selectedPaper === null) return;
+  paperDataStore.previousPaper();
+}
+function nextPaper() {
+  if (paperDataStore.selectedPaper === null) return;
+  paperDataStore.nextPaper();
+}
 </script>
 
 <template>
@@ -24,6 +48,8 @@ const { width: containerWidth, height: outerContainerHeight } =
     side="right"
     overlay
     :width="containerWidth"
+    v-touch-swipe.left="nextPaper"
+    v-touch-swipe.right="previousPaper"
   >
     <PaperInformation />
   </q-drawer>
