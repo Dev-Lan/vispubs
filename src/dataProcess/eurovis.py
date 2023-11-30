49e739b80cdb0a08f453f189317525b0fb5220ef
Devin Lange <devinscottlange@gmail.com>
2023-11-30 15:24:44 -0700

    get eurovis pre 2008

--- src/dataProcess/eurovis.py
+++ src/dataProcess/eurovis.py
@@ -12,32 +12,32 @@ def main():
 
     flat_list_urls = {'https://dblp.org/db/journals/cgf/cgf41.html#nr3'}
     url_list = [
-        'https://dblp.org/db/journals/cgf/cgf41.html#nr3', # 2022
-        'https://dblp.org/db/journals/cgf/cgf40.html#nr3', # 2021
-        'https://dblp.org/db/journals/cgf/cgf39.html#nr3',
-        'https://dblp.org/db/journals/cgf/cgf38.html#nr3',
-        'https://dblp.org/db/journals/cgf/cgf37.html#nr3', # 2018
-        'https://dblp.org/db/journals/cgf/cgf36.html#nr3', 
-        'https://dblp.org/db/journals/cgf/cgf35.html#nr3',
-        'https://dblp.org/db/journals/cgf/cgf34.html#nr3', # 2015
-        'https://dblp.org/db/journals/cgf/cgf33.html#nr3',
-        'https://dblp.org/db/journals/cgf/cgf32.html#nr3', # 2013
-        'https://dblp.org/db/journals/cgf/cgf31.html#nr3pt1',
-        'https://dblp.org/db/journals/cgf/cgf30.html#nr3', # 2011 (keynote/capstone)
-        'https://dblp.org/db/journals/cgf/cgf29.html#nr3',
-        'https://dblp.org/db/journals/cgf/cgf28.html#nr3',
-        'https://dblp.org/db/journals/cgf/cgf27.html#nr3', # 2008
-        # 'https://dblp.org/db/conf/vissym/eurovis2007.html',
-        # 'https://dblp.org/db/conf/vissym/eurovis2006.html',
-        # 'https://dblp.org/db/conf/vissym/eurovis2005.html',
-        # 'https://dblp.org/db/conf/vissym/vissym2004.html', # nested, skip inited
-        # 'https://dblp.org/db/conf/vissym/vissym2003.html',
-        # 'https://dblp.org/db/conf/vissym/vissym2002.html',
-        # 'https://dblp.org/db/conf/vissym/vissym2001.html',
-        # 'https://dblp.org/db/conf/vissym/vissym2000.html',
-        # 'https://dblp.org/db/conf/vissym/vissym1999.html'
-
+        # 'https://dblp.org/db/journals/cgf/cgf41.html#nr3', # 2022
+        # 'https://dblp.org/db/journals/cgf/cgf40.html#nr3', # 2021
+        # 'https://dblp.org/db/journals/cgf/cgf39.html#nr3',
+        # 'https://dblp.org/db/journals/cgf/cgf38.html#nr3',
+        # 'https://dblp.org/db/journals/cgf/cgf37.html#nr3', # 2018
+        # 'https://dblp.org/db/journals/cgf/cgf36.html#nr3', 
+        # 'https://dblp.org/db/journals/cgf/cgf35.html#nr3',
+        # 'https://dblp.org/db/journals/cgf/cgf34.html#nr3', # 2015
+        # 'https://dblp.org/db/journals/cgf/cgf33.html#nr3',
+        # 'https://dblp.org/db/journals/cgf/cgf32.html#nr3', # 2013
+        # 'https://dblp.org/db/journals/cgf/cgf31.html#nr3pt1',
+        # 'https://dblp.org/db/journals/cgf/cgf30.html#nr3', # 2011 (keynote/capstone)
+        # 'https://dblp.org/db/journals/cgf/cgf29.html#nr3',
+        # 'https://dblp.org/db/journals/cgf/cgf28.html#nr3',
+        # 'https://dblp.org/db/journals/cgf/cgf27.html#nr3', # 2008
+        'https://dblp.org/db/conf/vissym/eurovis2007.html', # 2007
+        'https://dblp.org/db/conf/vissym/eurovis2006.html',
+        'https://dblp.org/db/conf/vissym/eurovis2005.html',
+        'https://dblp.org/db/conf/vissym/vissym2004.html', # nested, skip invited
+        'https://dblp.org/db/conf/vissym/vissym2003.html', # also nested
+        'https://dblp.org/db/conf/vissym/vissym2002.html',
+        'https://dblp.org/db/conf/vissym/vissym2001.html',
+        'https://dblp.org/db/conf/vissym/vissym2000.html',
+        'https://dblp.org/db/conf/vissym/vissym1999.html'
     ]
+    pre2008 = True
     #get 1999 to 2007
     # pre 2007, this may be enough document.getElementsByClassName('publ-list')
     # https://api.crossref.org/${DOI}/transform/application/vnd.crossref.unixsd+xml
@@ -61,11 +61,29 @@ def main():
         # <br />
 
         soup = BeautifulSoup(page_html, 'html.parser')
-        key = url.split('#')[-1]
-        header = soup.select('#' + key)[0]
+        if not pre2008:
+            key = url.split('#')[-1]
+            header = soup.select('#' + key)[0]
 
         skip_sections = {'front matter', 'keynote', 'capstone'}
-        if nested:
+        if pre2008:
+            # document.getElementsByClassName('publ-list')
+            publication_list_of_lists = soup.select('.publ-list')
+            first = True
+            for publication_list in publication_list_of_lists:
+                # beautifulsoup, get number of children inside publication_list
+                length = len(publication_list.contents)      
+                if (length == 1):
+                    first = False
+                    # hacky way to skip the intro/invited talk of 2003/2004
+                    continue
+                for pub in publication_list.children:
+                    if first:
+                        first = False
+                        continue
+                    pub_info = parse_pub(pub)
+                    csv_string += pub_info + '\n'
+        elif nested:
             # curr = header.parent
             skip_next = False
             # while curr.next_sibling:
@@ -95,7 +113,7 @@ def main():
                 csv_string += parse_pub(pub) + '\n'
 
     # print(csv_string)
-    with open('eurovis.csv', 'w') as f:
+    with open('eurovis-pre-2008.csv', 'w') as f:
         f.write(csv_string)
     return
 
@@ -112,5 +130,7 @@ def parse_pub(pub):
         abstract = ''
         award = ''
         return ','.join([conference, year, title, doi, abstract, authors_string, award])
+
+
 if __name__ == '__main__':
     main()
