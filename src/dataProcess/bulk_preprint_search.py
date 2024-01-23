f2ca065aa847de701f2380eae5a473a5c886e016
Devin Lange <devinscottlange@gmail.com>
2024-01-22 18:37:43 -0700

    add search on osf with selenium

--- src/dataProcess/bulk_preprint_search.py
+++ src/dataProcess/bulk_preprint_search.py
@@ -6,6 +6,11 @@ import feedparser
 import time
 import sys
 import nltk
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 '''
 This scripts searches for preprint versions of papers using arxiv
 and osf api. It will check every row in the INPUT_METADATA_FILENAME.
@@ -19,12 +24,19 @@ INPUT_PAPER_LIST_FILENAME = '../../public/data/papers_test.csv'
 ROOT_FOLDER = '../../public/data/paperLinks/'
 
 def search_preprint_versions():
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+    # executable_path param is not needed if you updated PATH
+    browser = webdriver.Chrome(options=options)#, executable_path='YOUR_PATH/chromedriver.exe')
+
     with open(INPUT_PAPER_LIST_FILENAME, 'r') as file:
         reader = csv.reader(file)
         next(reader)  # Skip header row
         index = 0
         found_links = 0
         added_links = 0
+        found_arxiv = 0
+        found_osf = 0
         for row in reader:
             conf = row[0]
             year = row[1]
@@ -35,36 +47,75 @@ def search_preprint_versions():
             index += 1
             already_added = preprint_already_added(doi)
             if already_added:
+                print('\t✅ Skipping, already added')
                 found_links += 1
                 continue
 
-            link = search_osf_api(title)
-            if link is None:
-                link = search_arxiv_api(title)
-            if link is None:
-                link = search_bioarxiv_api(title)
-            if link:
-                print("\tFound")
+            link = search_arxiv_api(title)
+            if link is not None:
+                print("\t🍺 Found arXiv")
+            else:
+                link = search_osf_api(browser, title)
+
+            if link is not None:
+                print("\t🍺 Found OSF")
                 add_link_to_file(link, doi)
                 found_links += 1
                 added_links += 1
+            else:
+                print("\t❌ Not found")
 
-            wait_time = 5
-            for i in range(wait_time, 0, -1):
-                sys.stdout.write('\r')
-                sys.stdout.write('Waiting {} seconds...'.format(i))
-                sys.stdout.flush()
-                time.sleep(1)
-            sys.stdout.write('\r')
-            sys.stdout.write(' '*30)
-            sys.stdout.write('\r')
+            wait_and_print(5)
+    browser.quit()
     print('Finished searching for preprint versions.')
     print('Added', added_links, ', found', found_links, ', of', index, 'total papers.')
+    print('Found', found_osf, 'on OSF and', found_arxiv, 'on arXiv.')
 
-def search_osf_api(title):
-    # TODO
+def wait_and_print(seconds):
+    for i in range(seconds, 0, -1):
+        sys.stdout.write('\r')
+        sys.stdout.write('\tWaiting {} seconds...'.format(i))
+        sys.stdout.flush()
+        time.sleep(1)
+    sys.stdout.write('\r')
+    sys.stdout.write(' '*30)
+    sys.stdout.write('\r')
+    return
+
+def search_osf_api(browser, title):
+    try:
+        # Navigate to the OSF preprints search page
+        browser.get('https://osf.io/search?resourceType=Preprint')
+
+        # Find the search box element
+        search_box = WebDriverWait(browser, 10).until(
+            EC.presence_of_element_located((By.TAG_NAME, "input"))
+        )
+
+        # Type the title into the search box and press Enter
+        search_box.send_keys(title + Keys.RETURN)
+
+        # Wait 10 seconds, for results to hopefully load
+        wait_and_print(10)
+        # .until(
+            # EC.presence_of_element_located((By.TAG, "search-result"))
+        # )
+
+        # Extract the titles and links of the search results
+        results = browser.find_elements(By.TAG_NAME, 'a')
+        for result in results:
+            # print(result)
+            # print(result.get_attribute('innerHTML'))
+            # print(result.get_attribute('href'))
+            # print('='*20)
+            if close_enough(result.text, title):
+                return result.get_attribute('href')
         return None
-
+    except Exception as error:
+        # print the error
+        print('selemium error')
+        print(error)
+        return None
 
 def close_enough(s1, s2):
     d = nltk.edit_distance(s1, s2)
@@ -83,22 +134,13 @@ def search_arxiv_api(title):
 
     # Iterate over the entries
     for entry in feed.entries:
-        # If the entry's title is exactly the same as the title we're searching for, return its link
         candidate_title = entry.title.strip()
-        # print(candidate_title)
-        # print(title)
-        # dist = nltk.edit_distance(candidate_title, title)
-        # print(dist)
-        # print('---')
         if close_enough(candidate_title, title):
             return entry.link
 
-    # If no exact match is found, return None
+    # If no close enough match is found, return None
     return None
 
-def search_bioarxiv_api(title):
-    # TODO
-    return None
 
 def preprint_already_added(doi):
     filename = os.path.join(ROOT_FOLDER, doi)
@@ -114,6 +156,8 @@ def add_link_to_file(link, doi):
     with open(filename, 'r+') as file:
         # insert into the second line
         lines = file.readlines()
+        if len(lines) == 1:
+            lines[0] += '\n'
         lines.insert(1, 'Paper Preprint,' + link + ',paper\n')
         file.seek(0)
         file.writelines(lines)
