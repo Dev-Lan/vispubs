b63bff4268be616138f32d40556962766c65b682
dev-lan <devinscottlange@gmail.com>
2024-09-02 14:58:23 -0600

    update to convert xml to csv

--- src/dataProcess/parse_dblp_xml.py
+++ src/dataProcess/parse_dblp_xml.py
@@ -1,61 +1,93 @@
 from os import close
 from lxml import etree
+import pandas as pd
 
-def parse_large_xml_with_dtd(xml_file, dtd_file, output_filename):
-    # Load the DTD
-    with open(dtd_file, 'rb') as f:
-        dtd = etree.DTD(f)
+def parse_large_xml_with_dtd(xml_file, output_filename):
 
+    columns = ['Conference', 'Year', 'Title', 'DOI', 'Abstract', 'AuthorNames-Deduped', 'Award', 'Resources']
+    df = pd.DataFrame(columns=columns)
 
-    output_file = open(output_filename, 'w')
-
-    count = 0
     # Define a context for iteratively parsing the XML file
     context = etree.iterparse(xml_file, events=('end',), load_dtd=True, huge_tree=True)
 
-    vis_venues = set(['IEEE Trans. Vis. Comput. Graph.', 'Comput. Graph. Forum'])
-    vis_conferences = set(['CHI', 'IEEE VIS (Short Papers)', 'EuroVis (Short Papers)'])
-
     venue_types = set(['article', 'inproceedings'])
 
-    for event, elem in context:
+    included_venues = set(['CHI', 'Vis', 'EuroVis'])
 
-        if not dtd.validate(elem):  # Validate each element against the DTD
-            continue
+    count = 0
+
+    for event, elem in context:
 
         if elem.tag not in venue_types:
             continue
 
-        count += 1
-        if count % 100_000 == 0:
-            print(count / 1_000_000, 'M')
-
-
-        journal = elem.find('journal')
-        venue = elem.find('booktitle')
-
-        journal_text = journal.text if journal is not None else ''
-        venue_text = venue.text if venue is not None else ''
-        if journal_text == '' and venue_text == '':
+        venue = get_venue(elem)
+        if venue not in included_venues:
             continue
 
-        if journal_text not in vis_venues and venue_text not in vis_conferences:
+        title = get_text(elem, 'title')
+        if title == '' or title == 'Front Matter':
             continue
+        if title[-1] == '.':
+            title = title[:-1]
+        year = get_text(elem, 'year')
+        authors = ';'.join(get_text_list(elem, 'author'))
+        doi = get_doi(elem)
 
+        # print(venue, year, title, doi, abstract, authors, award, resources)
+        new_df = pd.DataFrame([{
+            'Conference': venue,
+            'Year': year,
+            'Title': title,
+            'DOI': doi,
+            'Abstract': '',
+            'AuthorNames-Deduped': authors,
+            'Award': '',
+            'Resources': ''
+        }], columns=columns)
+        df = pd.concat([df, new_df], ignore_index=True)
 
-        # TODO: extract info.
+    df.to_csv(output_filename, index=False)
+    return
 
-       
-        # Clear the element to free memory
-        # elem.clear()
-        # while elem.getprevious() is not None:
-        #     del elem.getparent()[0]
+def get_text(elem, tag_name):
+    sub_elem = elem.find(tag_name)
+    if sub_elem is None:
+        return None
+    text = sub_elem.text if sub_elem.text is not None else ''
+    return text.strip()
 
+def get_text_list(elem, tag_name):
+    sub_elems = elem.findall(tag_name)
+    sub_elems = [x.text.strip() for x in sub_elems]
+    return sub_elems
 
-    del context  # Ensure all memory is freed
-    output_file.close()
+def get_venue(elem):
+    booktitle = get_text(elem, 'booktitle')
+    if booktitle is not None and booktitle == 'CHI':
+        return 'CHI'
 
-# Replace 'your_large_file.xml' and 'your_dtd_file.dtd' with your actual file paths
-parse_large_xml_with_dtd('./input/dblp.xml', './input/dblp.dtd', './temp/vis_papers.xml')
+    journal = get_text(elem, 'journal')
+    year = int(get_text(elem, 'year'))
+    number = get_text(elem, 'number')
+    if journal == 'IEEE Trans. Vis. Comput. Graph.':
+        if number == '1':
+            return 'Vis'
+        else:
+            return 'TVCG'
+    if journal == 'Comput. Graph. Forum':
+        if year > 2007 and number == '3':
+            return 'EuroVis'
+        else:
+          return 'CGF'
+    return 'UNKNOWN'
 
+def get_doi(elem):
+    ee = get_text_list(elem, 'ee')
+    doi_org = 'https://doi.org/'
+    for link in ee:
+        if link.startswith('https://doi.org/'):
+            return link.replace(doi_org, '', 1)
+    return None
 
+parse_large_xml_with_dtd('./temp/dblp_filtered.xml', './temp/new_papers.csv')
