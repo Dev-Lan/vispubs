eaa5e023867aea3460c4850fd371080645184feb
dev-lan <devinscottlange@gmail.com>
2024-09-02 15:12:07 -0600

    brainstorm workflows

--- src/dataProcess/main.py
+++ src/dataProcess/main.py
@@ -1,3 +1,4 @@
+get new dblp data (handle early access papers)
 '''
 filter_dblp_xml.py # filters 4GB dblp xml to pubs from relevant files
 parse_dblp_xml.py # converts filtered dblp xml into csv and filters out some by conference
@@ -10,7 +11,16 @@ process_paper_links.py # to create stubs for any new paper resource file pages
 bulk_preprint_search.py # search for preprints
 update_paper_link_flags.py # to update the link flags column in paper list
 update_changelog.py # add to changelog file
-
 '''
 
+author submits homepage
+update_changelog.py # add to changelog file
 
+
+Author submits resource links
+update_paper_link_flags.py # to update the link flags column in paper list
+update_changelog.py # add to changelog file
+
+
+
+Add early access papers
