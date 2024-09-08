e55b89195781008ca1249b609990ccde130e98cd
dev-lan <devinscottlange@gmail.com>
2024-09-08 17:14:20 -0600

    refactor into main

--- src/dataProcess/combine.py
+++ src/dataProcess/combine.py
@@ -3,21 +3,30 @@
 import pandas as pd
 # Conference,Year,Title,DOI,Abstract,AuthorNames-Deduped,Award
 
-# Load the CSV files into pandas DataFrames
-df1 = pd.read_csv('VIS.csv')
-df2 = pd.read_csv('eurovis.csv')
-df3 = pd.read_csv('chi-filtered.csv')
+def main():
+  # Load the CSV files into pandas DataFrames
+  folder = './intermediate/'
+  df1 = pd.read_csv(folder + 'VIS.csv')
+  df2 = pd.read_csv(folder + 'eurovis.csv')
+  df3 = pd.read_csv(folder + 'chi-filtered.csv')
 
-# Combine the DataFrames vertically using concat
-combined_df = pd.concat([df1, df2, df3], axis=0, ignore_index=True)
+  # Combine the DataFrames vertically using concat
+  combined_df = pd.concat([df1, df2, df3], axis=0, ignore_index=True)
 
-# Sort the combined DataFrame by "Year", "Conference", and "Title" columns
-combined_df['Title_lower'] = combined_df['Title'].str.lower()
+  # Sort the combined DataFrame by "Year", "Conference", and "Title" columns
+  combined_df['Title_lower'] = combined_df['Title'].str.lower()
 
-combined_df = combined_df.sort_values(by=['Year', 'Conference', 'Title_lower'], ascending=[False, False, True], ignore_index=True)
+  combined_df = combined_df.sort_values(by=['Year', 'Conference', 'Title_lower'], ascending=[False, False, True], ignore_index=True)
 
-# drop "Title_lower" column
-combined_df = combined_df.drop(columns=['Title_lower'])
+  # drop "Title_lower" column
+  combined_df = combined_df.drop(columns=['Title_lower'])
 
-# Save the combined DataFrame to a new CSV file
-combined_df.to_csv('papers.csv', index=False)
+  # Add empty column Resources to end of DataFrame
+  combined_df['Resources'] = ""
+
+  # Save the combined DataFrame to a new CSV file
+  combined_df.to_csv('../../Public/data/papers.csv', index=False)
+
+
+if __name__ == '__main__':
+    main()
