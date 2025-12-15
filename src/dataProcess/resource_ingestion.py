8cefb9c43431d3d9d0233e38a5924194db7d6685
dev-lan <devinscottlange@gmail.com>
2025-12-15 14:28:43 -0700

    add user submitted paper resources

--- src/dataProcess/resource_ingestion.py
+++ src/dataProcess/resource_ingestion.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-'''
+"""
 Given an input csv file, this will update the resource files.
 
 The CSV file ahould have the following columns:
@@ -11,36 +11,39 @@ The CSV file ahould have the following columns:
 
 For each row, the DOI will be used to find the corresponding paper in the intermediate file.
 The resource will be added to the paper's resource list as long as the url is not already in the list.
-'''
+"""
 
 
-INPUT_FILENAME = './temp/vis24_resources.csv'
-ROOT_FOLDER = '../../public/data/paperLinks/'
+INPUT_FILENAME = "./temp/google_form_resources.csv"
+ROOT_FOLDER = "../../public/data/paperLinks/"
+
 
 def update_resources(input_filename):
-  #  load into pandas df
-  new_resources = pd.read_csv(input_filename)
-  #  iterate through the rows
-  for index, row in new_resources.iterrows():
-    doi = row['DOI']
-    name = row['name']
-    url = row['url']
-    icon = row['icon']
-    #  add the row to the resource files
-    add_row_to_resource_files(doi, name, url, icon)
-  return
-
-def add_row_to_resource_files(doi, name, url, icon):
-    with open(ROOT_FOLDER + doi, 'a+') as file:
-      #  Check if the url is already in the file
-      file.seek(0)  # Reset file pointer to the beginning
-      for line in file.readlines():
-        if url in line:
-          return
-      # seek to the end of the file
-      file.seek(0, 2)
-      file.write(f'\n{name},{url},{icon}')
+    #  load into pandas df
+    new_resources = pd.read_csv(input_filename)
+    #  iterate through the rows
+    for index, row in new_resources.iterrows():
+        doi = row["DOI"]
+        name = row["name"]
+        url = row["url"]
+        icon = row["icon"]
+        #  add the row to the resource files
+        add_row_to_resource_files(doi, name, url, icon)
     return
 
+
+def add_row_to_resource_files(doi, name, url, icon):
+    with open(ROOT_FOLDER + doi, "a+") as file:
+        #  Check if the url is already in the file
+        file.seek(0)  # Reset file pointer to the beginning
+        for line in file.readlines():
+            if url in line:
+                return
+        # seek to the end of the file
+        file.seek(0, 2)
+        file.write(f"\n{name},{url},{icon}")
+    return
+
+
 if __name__ == "__main__":
-  update_resources(INPUT_FILENAME)
+    update_resources(INPUT_FILENAME)
